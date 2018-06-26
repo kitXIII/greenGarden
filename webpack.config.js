290c3aeb4186764db6752b9e245dcc9833abd671
Nikita <mipo4t@gmail.com>
2018-06-26 14:21:28 +0400

    add webpack rule for correct fonts plugin, add yargs for getting webpack run mode in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,95 +1,88 @@
 const path = require('path');
+const yargs = require('yargs');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const Autoprefixer = require('autoprefixer');
 const MqPacker = require('css-mqpacker');
 
-module.exports = (env, options) => {
-  const devMode = options.mode !== 'production';
+const devMode = yargs.argv.mode !== 'production';
 
-  const conf = {
-    entry: {
-      vendors: './src/vendors.js',
-      main: './src/index.js',
-    },
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: './js/[name].js',
-      // publicPath: 'dist/',
-    },
-    devServer: {
-      overlay: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          // exclude: '/node_modules',
-        },
-        {
-          test: /\.min\.css$/,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
-        },
-        {
-          test: /\.(sa|sc)ss$/,
-          use: devMode
-            ? ['style-loader', 'css-loader', 'sass-loader']
-            : [
-              MiniCssExtractPlugin.loader,
-              {
-                loader: 'css-loader',
-                options: {
-                  minimize: true,
-                },
+const conf = {
+  entry: {
+    vendors: './src/vendors.js',
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/[name].js',
+    // publicPath: '../',
+  },
+  devServer: {
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // exclude: '/node_modules',
+      },
+      {
+        test: /\.min\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: devMode
+          ? ['style-loader', 'css-loader', 'sass-loader']
+          : [MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
               },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: [
-                    Autoprefixer,
-                    MqPacker,
-                  ],
-                },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  Autoprefixer,
+                  MqPacker,
+                ],
               },
-              'sass-loader',
-            ],
+            },
+            'sass-loader',
+          ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+            publicPath: '../fonts/',
+          },
         },
-        {
-          test: /\.(woff|woff2)$/,
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=./fonts/[name].[ext]',
-        },
-        {
-          test: /\.ttf$/,
-          loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]',
-        },
-        {
-          test: /\.eot$/,
-          loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=./fonts/[name].[ext]',
-        },
-        {
-          test: /\.svg$/,
-          loader: 'url-loader?limit=10000&mimetype=application/svg+xml&name=./fonts/[name].[ext]',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.sass', '.scss', '.css'],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: devMode ? './css/[name].css' : './css/[name].[hash].css',
-        chunkFilename: devMode ? './css/[id].css' : './css/[id].[hash].css',
-      }),
-      new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src', 'index.html'),
-        filename: path.join(__dirname, 'dist', 'index.html'),
-      }),
-      new CleanWebpackPlugin(['dist']),
+      },
     ],
-    devtool: devMode && 'eval-sourcemap',
-  };
-
-  return conf;
+  },
+  resolve: {
+    extensions: ['.js', '.sass', '.scss', '.css'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: devMode ? './css/[name].css' : './css/[name].[hash].css',
+      chunkFilename: devMode ? './css/[id].css' : './css/[id].[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: path.join(__dirname, 'dist', 'index.html'),
+    }),
+    new CleanWebpackPlugin(['dist']),
+  ],
+  devtool: devMode && 'eval-sourcemap',
 };
+
+module.exports = conf;
